29f4e44343b02c213d40bb0c761dc2403f4efc73
yogesh <yashbaghel613@gmail.com>
2024-01-25 04:53:19 -0800

    Exception handling globally and local

--- src/main/java/com/luv2Cod/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2Cod/demo/rest/StudentRestController.java
@@ -2,7 +2,9 @@
 package com.luv2Cod.demo.rest;
 import java.util.*;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,11 +29,35 @@ public class StudentRestController {
     }
     @GetMapping("/students")
     public List<PojoStudent > getStudent(){
+        //This data every time load when we cll it this is not a good that we use the @PostConstruct Annoatation which Load tha data once
+        //public List<PojoStudent> theStudents=new ArrayList<>(); 
+        //theStudents.add(new PojoStudent("Pooran","Patel"));
+        //theStudents.add(new PojoStudent("Prerak","singh"));
+        //theStudents.add(new PojoStudent("Palak","Singh"));
         return theStudents;
         
     } 
     @GetMapping("/students/{studentId}")
     public PojoStudent getStudents(@PathVariable int studentId){
+
+        //Error handle
+        if((studentId>=theStudents.size())|| (studentId<0)){
+            throw new StudentNotFoundException("student Id not found"+studentId);
+        }
         return theStudents.get(studentId);
     }
+
+
+    // Add an Exception handle using @ExceptionHandler 
+    //THIS ERROR FOR PARTICULAR ERROR
+    //@ExceptionHandler
+    //public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+       // StudentErrorResponse error=new StudentErrorResponse();
+       // error.setStatus(HttpStatus.NOT_FOUND.value());
+       // error.setMessage(exc.getMessage());
+       // error.setTimeStamp(System.currentTimeMillis());
+       // return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+   // }
+   
 }
